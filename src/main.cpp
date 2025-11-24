7cf86cf708bcd2ea46d13c10735019ec55e6b7a1
Mehdi <mehdi.el.mestari.pro@gmail.com>
2025-11-24 12:41:36 +0100

    build(setup): SFML 3 configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,41 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <optional> // Requis par SFML 3 : pollEvent retourne maintenant un std::optional
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    // --- 1. INITIALISATION DE LA FENÊTRE ---
+    // En SFML 3, la taille (Vector2u) doit être passée entre accolades {}.
+    // Cela crée implicitement un objet sf::Vector2u(800, 600).
+    auto mode = sf::VideoMode({800, 600});
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    sf::RenderWindow window(mode, "Code Fighter - Pre-Alpha");
+
+    // --- 2. BOUCLE DE JEU (GAME LOOP) ---
+    // Cette boucle tourne tant que la fenêtre est ouverte.
+    while (window.isOpen()) {
+
+        // --- 3. GESTION DES INPUTS (POLLING) ---
+        // window.pollEvent() ne bloque pas le programme.
+        // Elle retourne un std::optional contenant l'événement s'il y en a un,
+        // ou "std::nullopt" (vide) s'il n'y en a plus.
+        while (const std::optional event = window.pollEvent()) {
+
+            // SFML 3 remplace l'enum "event.type" par une méthode template ".is<T>()".
+            // C'est plus robuste ("type-safe"). Ici, on vérifie si l'utilisateur a cliqué sur la croix.
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        // --- 4. RENDU GRAPHIQUE ---
+        // A. Nettoyage : On efface l'image précédente avec une couleur de fond (Gris foncé).
+        window.clear(sf::Color(50, 50, 50));
+
+        // B. Dessin : C'est ici qu'on affichera les personnages et cartes plus tard.
+        // window.draw(playerSprite);
+
+        // C. Affichage : On bascule le buffer caché vers l'écran visible (Double Buffering).
+        window.display();
     }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
